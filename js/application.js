7c06c3f1b9413d7780659a4b03d26ff71635c9c8
Andy Gross <andogro@gmail.com>
2016-01-10 17:46:58 -0700

    made relative

--- js/application.js
+++ js/application.js
@@ -1,22 +1,42 @@
 $(document).ready(function() {
 
 function removePic() {
-     $('.picture > img').remove();
+     $('.top > img').remove();
 }
 
 $('.pic1').on('click', function() {
 removePic();
-$('.picture').append('<img src ="images/andy-1.jpg" alt= "Andy Gross">');
+$('.top').append('<img src ="images/andy-1.jpg" alt= "Andy Gross" class="picture">');
 });
 
 $('.pic2').on('click', function() {
 removePic();
-$('.picture').append('<img src ="images/andy_fam.jpg" alt="Family">');
+$('.top').append('<img src ="images/andy_fam.jpg" alt="Family" class="picture">');
 });
 
 $('.pic3').on('click', function() {
 removePic();
-$('.picture').append('<img src ="images/half-life.jpg" alt ="Half Life">');
+$('.top').append('<img src ="images/half-life.jpg" alt ="Half Life" class="picture">');
+});
+
+$('.pic4').on('click', function() {
+removePic();
+$('.top').append('<img src ="images/boulder_1.jpg" alt ="Hiking in Boulder" class="picture">');
+});
+
+$('.pic5').on('click', function() {
+removePic();
+$('.top').append('<img src ="images/cooking.jpg" alt ="Cooking" class="picture">');
+});
+
+$('.pic6').on('click', function() {
+removePic();
+$('.top').append('<img src ="images/frisbee_golf.jpg" alt ="Frisbee Golf" class="picture">');
+});
+
+$('.pic7').on('click', function() {
+removePic();
+$('.top').append('<img src ="images/frisbee_golf.jpg" alt ="Frisbee Golf" class="picture">');
 });
 
 });
